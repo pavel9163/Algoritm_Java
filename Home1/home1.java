c17c07a02b75162d0f5375c372e62beaae0413d0
pavel9163 <129773646+pavel9163@users.noreply.github.com>
2023-08-16 15:36:06 +0300

    Update home1.java

--- Home1/home1.java
+++ Home1/home1.java
@@ -1,51 +1,84 @@
 public class HeapSort {
-    public static void heapify(int[] arr, int n, int i) {
-        int largest = i;
-        int leftChild = 2 * i + 1;
-        int rightChild = 2 * i + 2;
 
-        if (leftChild < n && arr[leftChild] > arr[largest]) {
-            largest = leftChild;
-        }
+  /**
+   * Метод запуска сортировки массива
+   * @param args
+   */
+  public static void main(String[] args) {
+    int[] arr = {1234, 4, 7, 2, 1, -3, 0, 567, 45, 90, 34,2, 234};
 
-        if (rightChild < n && arr[rightChild] > arr[largest]) {
-            largest = rightChild;
-        }
+    heapSort(arr);
 
-        if (largest != i) {
-            int temp = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = temp;
+    for (int i = 0; i < arr.length; i++)
+      System.out.print(arr[i] + " ");
+  }
 
-            heapify(arr, n, largest);
-        }
+  /**
+   * Метод создания дерева массива
+   * @param arr
+   */
+  public static void heapSort(int[] arr) {
+    //длина массива
+    int n = arr.length;
+
+    //создаём дерево, построение кучи (перегруппируем массив)
+    for(int i  = n / 2 - 1; i >= 0; i--)
+      heapify(arr, i , n);
+
+    //Делаем сортировку массива, уже отсортированного дерева,
+    //Один за другим извлекаем элементы из кучи
+    for (int i = n - 1; i >= 0; i--){
+
+      // Перемещаем текущий корень в конец
+      int temp = arr[i];
+      arr[i] = arr[0];
+      arr[0] = temp;
+
+      // Вызываем процедуру heapify на уменьшенной куче
+      heapify(arr, 0, i);
     }
 
-    public static void heapSort(int[] arr) {
-        int n = arr.length;
+  }
 
-        // Build a max heap
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(arr, n, i);
-        }
+  /**
+   * Главный метод в котором будет меняться структура данных
+   * так чтоб родитель был максимальны элементом по сравнению
+   * со своими детьми в дереве
+   * @param arr
+   * @param i
+   * @param n
+   */
+  private static void heapify(int[] arr, int i, int n) {
+    //левый ребёнок
+    int l = i * 2 + 1;
 
-        // Extract elements from the heap one by one
-        for (int i = n - 1; i >= 0; i--) {
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
+    //правый ребёнок
+    int r = i * 2 + 2;
 
-            heapify(arr, i, 0);
-        }
+    //Инициализируем наибольший элемент как корень
+    int largest = i;
+
+    //Проверка чтоб дети не стали больше чем родители
+    //Если левый дочерний элемент больше корня
+    if(l < n && arr[l] > arr[largest])
+      largest = l;
+
+    //Если правый дочерний элемент больше,
+    // чем самый большой элемент на данный момент
+    if(r < n && arr[r] > arr[largest])
+      largest = r;
+
+    //Если, ребёнок оказался больше родителя, то делаем обмен,
+    //ребёнка с родителем. Если самый большой элемент не корень
+    if (i != largest){
+      int temp = arr[i];
+      arr[i] = arr[largest];
+      arr[largest] = temp;
+
+      //Проверяем ещё раз чтоб дети были меньше чем родители,
+      //если, вдруг у детей есть свои дети
+      //Рекурсивно преобразуем в двоичную кучу затронутое поддерево
+      heapify(arr, largest, n);
     }
-
-    public static void main(String[] args) {
-        int[] arr = {12, 11, 13, 5, 6, 7};
-        heapSort(arr);
-
-        System.out.print("Отсортированный массив: ");
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
-    }
+  }
 }
